6ddbc8f76cb962c0364f2c905ab666f532d7b2e1
mal <oscar.shrimpton.personal@gmail.com>
2020-04-07 16:54:10 +0100

    delete comments route and some more validation

--- src/controllers/post-comment.controller.ts
+++ src/controllers/post-comment.controller.ts
@@ -15,6 +15,7 @@ import {
   patch,
   post,
   requestBody,
+  HttpErrors,
 } from '@loopback/rest';
 import {authenticate} from '@loopback/authentication';
 import {SecurityBindings, UserProfile, securityId} from '@loopback/security';
@@ -26,12 +27,13 @@ import {
   Comment,
   CommentWithRelations,
 } from '../models';
-import {PostRepository, PostVoteRepository, CommentVoteRepository} from '../repositories';
+import {PostRepository, PostVoteRepository, CommentVoteRepository, CommentRepository} from '../repositories';
 import {LOGGED_IN} from '../spec';
 
 export class PostCommentController {
   constructor(
     @repository(PostRepository) protected postRepository: PostRepository,
+    @repository(CommentRepository) protected commentRepository: CommentRepository,
     @repository(PostVoteRepository) protected postVoteRepository: PostVoteRepository,
     @repository(CommentVoteRepository) protected commentVoteRepository: CommentVoteRepository,
   ) { }
@@ -116,6 +118,13 @@ export class PostCommentController {
     @inject(SecurityBindings.USER)
     profile: UserProfile,
   ): Promise<Comment> {
+    if (comment.replyTo) {
+      const parent = await this.commentRepository.findById(comment.replyTo);
+
+      if (parent.postId !== comment.postId)
+        throw new HttpErrors.BadRequest("postId should match postId of parent comment");
+    }
+
     return this.postRepository.comments(id).create({
       ...comment,
       userName: profile[securityId],
@@ -123,18 +132,39 @@ export class PostCommentController {
     });
   }
 
-  // @del('/posts/{id}/comments', {
-  //   responses: {
-  //     '200': {
-  //       description: 'Post.Comment DELETE success count',
-  //       content: {'application/json': {schema: CountSchema}},
-  //     },
-  //   },
-  // })
-  // async delete(
-  //   @param.path.number('id') id: number,
-  //   @param.query.object('where', getWhereSchemaFor(Comment)) where?: Where<Comment>,
-  // ): Promise<Count> {
-  //   return this.postRepository.comments(id).delete(where);
-  // }
+  @del('/posts/{id}/comments', {
+    security: LOGGED_IN,
+    responses: {
+      '200': {
+        description: 'Post.Comment DELETE success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  @authenticate('jwt')
+  async delete(
+    @param.path.number('id') id: number,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {
+              commentId: {type: 'number'},
+            },
+          },
+        },
+      },
+    }) comment: {commentId: number},
+    @inject(SecurityBindings.USER)
+    profile: UserProfile,
+  ): Promise<Count> {
+    const post = await this.postRepository.findById(id, {include: [{relation: "subforum"}]});
+    if (post.subforum.ownerName != profile[securityId])
+      throw new HttpErrors.BadRequest("You don't own the subforum of this post");
+
+    return this.postRepository.comments(id).delete({
+      commentId: comment.commentId
+    });
+  }
 }
